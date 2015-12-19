2a97334b96c1c4171ad986a661f0905a2441c07b
dartchemist <1qa2ws3ed>
2015-12-19 23:07:56 +0200

    Commit dialog service

---
+++ HelpDriverBuddy.UniversalWindowsClient/Infrastructure/Dialogs/DialogService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace HelpDriverBuddy.UniversalWindowsClient.Infrastructure.Dialogs
+{
+    public class DialogService : IDialogService
+    {
+        public async Task ShowMessage(string message)
+        {
+            var messageBox = new MessageDialog(message);
+            messageBox.Commands.Add(new UICommand
+            {
+                Id = 0,
+                Label = "Ok"
+            });
+
+            await messageBox.ShowAsync();
+        }
+    }
+}
